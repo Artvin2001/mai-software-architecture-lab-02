0cd7f2c92d000bda994a377ee15681d4c36e4c95
Artvin2001 <65854054+Artvin2001@users.noreply.github.com>
2023-12-14 10:59:05 +0300

    Add project structure

---
+++ user_service/main.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='User Service',
+    description='Сервис авторизации',
+    docs_url='/',
+)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--host", type=str, dest="host", default="127.0.0.1")
+    parser.add_argument("--port", type=int, dest="port", default=8000)
+    arguments = parser.parse_args()
+
+    uvicorn.run(app, host=arguments.host, port=arguments.port)
